2f97246b824017de5e4d42028168e6e626aafd08
Ritikjain91 <jainritik829@gmail.com>
2024-07-26 13:12:32 +0530

    fully responsive code

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ export default function Example() {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              {/* Mobile menu button */}
+              
               <Disclosure.Button className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="sr-only">Open main menu</span>
                 <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
@@ -20,7 +20,7 @@ export default function Example() {
               <div className="flex flex-shrink-0 items-center text-white font-bold">Wellness Treat</div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  {/* Add navigation links here */}
+               
                 </div>
               </div>
             </div>
@@ -80,7 +80,7 @@ export default function Example() {
 
         <Disclosure.Panel className="sm:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
-            {/* Add mobile menu links here */}
+           
           </div>
         </Disclosure.Panel>
       </Disclosure>
@@ -88,7 +88,7 @@ export default function Example() {
         <img
           src={Movie}
           alt="Placeholder"
-          className="w-full h-[50vh] absolute top-30 rounded-lg shadow-lg"
+          className="w-[190vh] h-[50vh] absolute top-30 rounded-lg shadow-lg"
         />
         <div className="absolute text-center text-black">
           <h1 className='text-4xl  font-bold'>Discover Your Inner Peace</h1>
